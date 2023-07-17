6120f42be78a850689d8227f46b19db8701d6b81
HyungCheolSim <sim34122@gmail.com>
2023-07-17 23:29:43 +0900

    구현을 위해 수정한 역할 외 코드 shop.java, ShopRepository
    머지 시 이쪽 코드는 선택하지 않을 예정

--- src/main/java/com/sprta/deliveryproject/entity/Shop.java
+++ src/main/java/com/sprta/deliveryproject/entity/Shop.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Shop {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shop_id", unique = true, updatable = false, nullable = false)
     private Long id;
 
     @Column(name="shopname",nullable = false)
