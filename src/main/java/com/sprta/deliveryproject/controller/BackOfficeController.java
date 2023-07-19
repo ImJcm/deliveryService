94fdfdd042dd40997b02264d0e29561f1696f1f3
JungHoon <meslee70@gmail.com>
2023-07-19 21:09:05 +0900

    backOffice에서 가게 메뉴 추가 기능, 특정가게 메뉴들 조회, 가게 정보 조회

---
+++ src/main/java/com/sprta/deliveryproject/controller/BackOfficeController.java
@@ -0,0 +1,28 @@
+package com.sprta.deliveryproject.controller;
+
+import com.sprta.deliveryproject.dto.ApiResponseDto;
+import com.sprta.deliveryproject.dto.MenuRequestDto;
+import com.sprta.deliveryproject.security.UserDetailsImpl;
+import com.sprta.deliveryproject.security.UserDetailsServiceImpl;
+import com.sprta.deliveryproject.service.BackOfficeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class BackOfficeController {
+
+    private BackOfficeService backOfficeService;
+
+    public BackOfficeController(BackOfficeService backOfficeService){
+        this.backOfficeService = backOfficeService;
+    }
+
+    @PostMapping("/shops/{id}/menus")       //특정가게 메뉴 추가(해당 가게 주인만 가능)
+    public ResponseEntity<ApiResponseDto> AddMenu(@PathVariable Long id, @RequestBody MenuRequestDto menuRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails)
+    {
+        return backOfficeService.AddMenu(id,menuRequestDto,userDetails);
+
+    }
+}
--- src/main/java/com/sprta/deliveryproject/controller/ShopController.java
+++ src/main/java/com/sprta/deliveryproject/controller/ShopController.java
@@ -25,12 +25,17 @@ public class ShopController
 {
     private final ShopService shopService;
 
-    @GetMapping("/shops/category/{id}")
+    @GetMapping("/shops/category/{id}")     //카테고리 별 가게 조회
     public ResponseEntity<ApiResponseDto> getCategoryShops(@PathVariable Long id) {
         return shopService.getCategoryShops(id);
     }
-    @GetMapping("/shops/{id}")
+    @GetMapping("/shops/{id}")          //특정가게 조회
     public ResponseEntity<ApiResponseDto> getShops(@PathVariable Long id) {
         return shopService.getDetailShops(id);
     }
+    @GetMapping("/shops/{id}/menu")     //특정가게 메뉴조회
+    public ResponseEntity<ApiResponseDto> getMenus(@PathVariable Long id){   //특정가게 메뉴조회
+        return shopService.getMenus(id);
+    }
+
 }
