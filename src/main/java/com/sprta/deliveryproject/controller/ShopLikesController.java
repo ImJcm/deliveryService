f103b9d16126c361a5e351114a66d85db7e86427
HyungCheolSim <sim34122@gmail.com>
2023-07-20 03:50:02 +0900

    리뷰 수정에 따른 ShopResponseDto.java 수정, 자잘한 정리

--- src/main/java/com/sprta/deliveryproject/controller/ShopLikesController.java
+++ src/main/java/com/sprta/deliveryproject/controller/ShopLikesController.java
@@ -4,9 +4,6 @@ package com.sprta.deliveryproject.controller;
 // 후에 shopController 에 합쳐질 수 있다.
 
 import com.sprta.deliveryproject.dto.ApiResponseDto;
-import com.sprta.deliveryproject.dto.ShopLikeResponseDto;
-import com.sprta.deliveryproject.dto.ShopResponseDto;
-import com.sprta.deliveryproject.entity.Shop;
 import com.sprta.deliveryproject.security.UserDetailsImpl;
 import com.sprta.deliveryproject.service.ShopLikesService;
 import lombok.RequiredArgsConstructor;
@@ -15,32 +12,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/shop")
 public class ShopLikesController {
     private final ShopLikesService shopLikesService;
-    
+
     //찜하기
-    @PostMapping("/{shopId}/likes")
-    public ResponseEntity<ApiResponseDto> createShopLikes(@PathVariable Long shopId, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        shopLikesService.createShopLikes(shopId,userDetails.getMember());
-        return ResponseEntity.ok().body(new ApiResponseDto("해당 가게를 찜했습니다.",HttpStatus.OK.value()));
+    @PostMapping("/{shop_id}/likes")
+    public ResponseEntity<ApiResponseDto> createShopLikes(@PathVariable(value = "shop_id") Long shopId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        shopLikesService.createShopLikes(shopId, userDetails.getMember());
+        return ResponseEntity.ok().body(new ApiResponseDto("가게를 찜했습니다.", HttpStatus.OK.value()));
     }
 
     //찜하기 취소
-    @DeleteMapping("likes/{id}")
-    public ResponseEntity<ApiResponseDto> deleteShopLikes(@PathVariable Long id,@AuthenticationPrincipal UserDetailsImpl userDetails){
-        shopLikesService.deleteShopLikes(id,userDetails.getMember());
-        return ResponseEntity.ok().body(new ApiResponseDto("해당 가게 찜을 취소했습니다.", HttpStatus.OK.value()));
+    @DeleteMapping("likes/{shop_like_id}")
+    public ResponseEntity<ApiResponseDto> deleteShopLikes(@PathVariable(value = "shop_like_id") Long shopLikeId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        shopLikesService.deleteShopLikes(shopLikeId, userDetails.getMember());
+        return ResponseEntity.ok().body(new ApiResponseDto("가게 찜을 취소했습니다.", HttpStatus.OK.value()));
     }
 
 
     //특정 멤버가 찜한(좋아요) 가게 조회
     @GetMapping("/member/{member_id}/likes")
-    public List<ShopLikeResponseDto> getMemberlikeshop(@PathVariable Long member_id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return shopLikesService.getMemberlikeshop(member_id, userDetails.getMember());
+    public ResponseEntity<ApiResponseDto> getMemberShopLikes(@PathVariable(value = "member_id") Long memberId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), "특정 멤버가 찜한 가게 목록 조회 성공.", shopLikesService.getMemberlikeshop(memberId, userDetails.getMember())));
     }
 }
