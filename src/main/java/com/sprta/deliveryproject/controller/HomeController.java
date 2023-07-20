196dee1d5ba5dcbec26a2d3e3a2be50799b7b34c
ImJcm <wjsckdals0909@naver.com>
2023-07-20 11:18:36 +0900

    FEATURE : Add Main Page Frontend

--- src/main/java/com/sprta/deliveryproject/controller/HomeController.java
+++ src/main/java/com/sprta/deliveryproject/controller/HomeController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        System.out.println("index 호출");
-        model.addAttribute("username", userDetails.getUsername());
+        model.addAttribute("memberId", userDetails.getMember().getId());
+        model.addAttribute("memberProfilename",userDetails.getMember().getProfilename());
         return "index";
     }
 }
